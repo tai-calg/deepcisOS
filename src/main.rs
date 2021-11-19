8f25aba59a5945fdea738eaa0839321424666f45
tai-calg <bsns.123456.clg@gmail.com>
2021-11-20 00:54:00 +0900

    new day1 from blog-os

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,32 @@
 #![no_std]
 #![no_main]
 #![feature(asm)]
-#![feature(abi_efiapi)]
-#![feature(proc_macro_hygiene)]
+#![feature(start)]
 
-extern crate utf16_literal;
-use utf16_literal::utf16;
+
+
 
 use core::panic::PanicInfo;
+use core;
 
-mod uefi;
-use uefi::Status;
 
-static mut GLOBAL_STDOUT: Option<&uefi::SimpleTextOutputProtocol> = None;
 
+//no-mangleはRustにはないシステムプログラミングに必要な
+//Cライブラリと同等の機能を使えるようにする。e.g. memcpyなど
 #[no_mangle]
-pub extern "efiapi" fn efi_main(
-    _image: uefi::Handle,
-    stable: &'static uefi::SystemTable,
-) -> Status {
-    let stdout = stable.stdout();
-    unsafe {
-        GLOBAL_STDOUT = Some(stdout);
-    }
-    stdout.reset(false);
-    //panic!("");
-    stdout.output_string(utf16!("Hello, World!").as_ptr());
-
+pub extern "C" fn _start() -> ! {
     loop {}
 }
+#[no_mangle]
+fn hlt() {
+    unsafe {
+        asm!("hlt");
+    }   
+}
+
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    unsafe {
-        if let Some(s) = GLOBAL_STDOUT {
-            s.output_string(utf16!("panic").as_ptr());
-        }
-    }
+
     loop {}
 }
